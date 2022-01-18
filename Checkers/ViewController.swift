a4780f50cadad895ad546f0fbea267c3c5737fc7
Ilya Sinitsyn <Best45@inbox.ru>
2022-01-18 08:59:09 +0500

    Initial Commit

---
+++ Checkers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Илья Синицын on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
