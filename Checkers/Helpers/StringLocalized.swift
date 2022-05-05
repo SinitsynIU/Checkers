a6aacb4f332670e63af71ffeb1b30f478f7461a2
Ilya Sinitsyn <Best45@inbox.ru>
2022-05-05 23:13:27 +0500

    05.05.2022

---
+++ Checkers/Helpers/StringLocalized.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Checkers
+//
+//  Created by Илья Синицын on 03.04.2022.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        guard let url = Bundle.main.url(forResource: UserDefaults.standard.string(forKey: "language"), withExtension: "lproj"),
+              let langBandel = Bundle(url: url) else {
+        return self
+        }
+        return NSLocalizedString(self, bundle: langBandel, comment: "")
+    }
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Checkers' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Checkers
+  pod 'IQKeyboardManagerSwift'
+  pod 'LTMorphingLabel'
+  pod "GlitchLabel"
+  pod 'SwiftEntryKit'
+end
